8d8d4cdb50a7d6a4a3a2d237e77ba25bdbb6d453
roblesdotdev <httpsnip@gmail.com>
2023-07-03 22:44:15 -0300

    project card

--- app/components/sections/projects.tsx
+++ app/components/sections/projects.tsx
@@ -1,3 +1,4 @@
+import { ArrowUpRightIcon } from 'lucide-react'
 import type { Project } from '~/types'
 
 export default function ProjectsSection({ projects }: { projects: Project[] }) {
@@ -13,10 +14,29 @@ export default function ProjectsSection({ projects }: { projects: Project[] }) {
         {projects.map((project, k) => (
           <div
             key={k}
-            className="flex flex-col gap-1 rounded-md border border-white/10 bg-white/[0.03] p-4"
+            className="flex flex-col items-start gap-1 rounded-md bg-white/[0.03] p-4 ring-[1px] ring-white/10"
           >
-            <h1 className="font-bold">{project.title}</h1>
-            <p className="line-clamp-2 text-fg-muted">{project.subtitle}</p>
+            <button className="underline-offset-4 transition hover:underline">
+              <h1 className="font-bold">
+                <div className="flex items-center gap-1">
+                  {project.title}
+                  <ArrowUpRightIcon className="h-4 w-4 text-fg-muted" />
+                </div>
+              </h1>
+            </button>
+            <p className="line-clamp-2 text-start text-fg-muted">
+              {project.subtitle}
+            </p>
+            <ul className="mt-1 flex items-center gap-2">
+              {project.tags.map((tag, idx) => (
+                <li
+                  key={idx}
+                  className="rounded-sm bg-white/5 px-2 py-1 text-xs"
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
           </div>
         ))}
       </div>
