342c5613f904695d4df4079015fa5b151798b6c0
roblesdotdev <httpsnip@gmail.com>
2023-07-05 00:30:44 -0300

    footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,8 +1,23 @@
 export default function Footer() {
   return (
-    <footer className="mt-16">
+    <footer className="mt-32 flex flex-col gap-6 text-center">
       <p className="text-center">
         Aldo R. Robles - {new Date().getFullYear()} - Argentina
+      </p>
+      <p className="text-sm">
+        Este sitio está construido con{' '}
+        <a href="https://remix.run" target="_blank" rel="noreferrer">
+          Remix.js
+        </a>{' '}
+        y{' '}
+        <a href="https://tailwindcss.com" target="_blank" rel="noreferrer">
+          TailwindCSS
+        </a>{' '}
+        y desplegado en{' '}
+        <a href="https://fly.io" target="_blank" rel="noreferrer">
+          Fly.io
+        </a>{' '}
+        con Docker.
       </p>
     </footer>
   )
--- app/components/sections/contact.tsx
+++ app/components/sections/contact.tsx
@@ -5,9 +5,14 @@ export default function ContactSection() {
       <p>
         Si te interesa conocer más sobre mi perfil, estaré encantado de recibir
         tus noticias. Por favor, no dudes en contactarme a través de{' '}
-        <a href="mailto:robles.contactme@gmail.com">mi correo</a>. Estoy
-        dispuesto a analizar nuevas oportunidades profesionales y emocionado por
-        las posibilidades que puedan surgir.
+        <a
+          href="mailto:robles.contactme@gmail.com"
+          className="text-fg underline underline-offset-4 transition hover:text-fg-muted"
+        >
+          robles.contactme@gmail.com
+        </a>
+        . Estoy dispuesto a analizar nuevas oportunidades profesionales y
+        emocionado por las posibilidades que puedan surgir.
       </p>
     </div>
   )
