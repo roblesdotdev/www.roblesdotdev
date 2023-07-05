826bdec74e968f27a075a78f3b7c74fb86ab6c83
roblesdotdev <httpsnip@gmail.com>
2023-07-05 00:48:29 -0300

    footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,23 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="mt-32 flex flex-col gap-6 text-center">
-      <p className="text-center">
-        Aldo R. Robles - {new Date().getFullYear()} - Argentina
-      </p>
-      <p className="text-sm">
-        Este sitio está construido con{' '}
-        <a href="https://remix.run" target="_blank" rel="noreferrer">
-          Remix.js
-        </a>{' '}
-        y{' '}
-        <a href="https://tailwindcss.com" target="_blank" rel="noreferrer">
-          TailwindCSS
-        </a>{' '}
-        y desplegado en{' '}
-        <a href="https://fly.io" target="_blank" rel="noreferrer">
-          Fly.io
-        </a>{' '}
-        con Docker.
+    <footer className="mt-24 flex flex-col gap-4 py-8 text-center">
+      <p>
+        &copy; Aldo R. Robles - {new Date().getFullYear()} - Tinogasta,
+        Argentina
       </p>
     </footer>
   )
